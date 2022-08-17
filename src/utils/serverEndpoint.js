9a2b07759b9a23fa8363f54fd4e4160333883f58
David <r.d.jorgensen@gmail.com>
2022-08-17 16:28:53 -0700

    extra check

--- src/utils/serverEndpoint.js
+++ src/utils/serverEndpoint.js
@@ -1,4 +1,4 @@
-const serverEndpoint = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development')
+const serverEndpoint = ((!process.env.NODE_ENV || process.env.NODE_ENV === 'development') && process.env.DEPLOYED === "false")
   ? `http://localhost:4000`
   : process.env.REACT_APP_PRODUCTION_ENDPOINT;
 
