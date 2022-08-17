171db32472953a089774b70724b82a46f0c10be3
David <r.d.jorgensen@gmail.com>
2022-08-17 16:00:55 -0700

    local host is no longer secure

--- src/utils/serverEndpoint.js
+++ src/utils/serverEndpoint.js
@@ -1,5 +1,5 @@
 const serverEndpoint = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development')
-  ? `https://localhost:4000`
+  ? `http://localhost:4000`
   : process.env.REACT_APP_PRODUCTION_ENDPOINT;
 
 export default serverEndpoint;
