be81d5a73cb374dec93665ac0d81e418a13211de
David <r.d.jorgensen@gmail.com>
2022-08-19 13:46:12 -0700

    typo fix

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import initFacebookSdk from './services/initFacebookSdk';
-import App from './App';;
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
